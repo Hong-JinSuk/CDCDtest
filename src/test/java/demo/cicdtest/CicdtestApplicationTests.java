eea9a5c15db65f1bbf98e3398614f9031470974d
RealTone <realtone98@gmail.com>
2024-07-09 22:15:09 +0900

    init

---
+++ src/test/java/demo/cicdtest/CicdtestApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.cicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
